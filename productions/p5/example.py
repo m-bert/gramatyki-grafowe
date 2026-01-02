996712963074d5cd829f412705fb26ed072ac7c3
Michał <michal.bert@swmansion.com>
2026-01-02 19:37:08 +0100

    Add borders flag

--- productions/p5/example.py
+++ productions/p5/example.py
@@ -10,8 +10,6 @@ from productions.p5.p5 import P5
 output_dir = "./productions/p5/outputs"
 os.makedirs(output_dir, exist_ok=True)
 
-# Test 1: Simple quadrilateral
-
 graph = HyperGraph()
 production = P5()
 
@@ -25,14 +23,14 @@ n6 = graph.add_node(2, 1, label="6")
 n7 = graph.add_node(1, 2, label="7")
 n8 = graph.add_node(0, 1, label="8")
 
-graph.add_edge(n1, n5)
-graph.add_edge(n5, n2)
-graph.add_edge(n2, n6)
-graph.add_edge(n6, n3)
-graph.add_edge(n3, n7)
-graph.add_edge(n7, n4)
-graph.add_edge(n4, n8)
-graph.add_edge(n8, n1)
+graph.add_edge(n1, n5, is_border=True)
+graph.add_edge(n5, n2, is_border=True)
+graph.add_edge(n2, n6, is_border=True)
+graph.add_edge(n6, n3, is_border=True)
+graph.add_edge(n3, n7, is_border=True)
+graph.add_edge(n7, n4, is_border=True)
+graph.add_edge(n4, n8, is_border=True)
+graph.add_edge(n8, n1, is_border=True)
 
 
 q = graph.add_hyperedge([n1, n2, n3, n4], label="Q")
